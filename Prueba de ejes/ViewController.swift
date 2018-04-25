0c5b3217622fcb87d95ff0b7a178eeea37fe0c98
Ivan Almada <ivan.almada@green-light.com>
2018-04-25 18:46:11 +0200

    Agrega plano! No se si va a funcionar!

--- Prueba de ejes/ViewController.swift
+++ Prueba de ejes/ViewController.swift
@@ -74,4 +74,47 @@ class ViewController: UIViewController {
             elemento.removeFromParentNode()
         }
     }
+
+    @IBAction func agregarPlano(_ sender: Any) {
+        // create a new scene
+        // let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene(named: "art.scnassets/te5.scn")!
+
+        // create and add a camera to the scene
+        let cameraNode = SCNNode()
+        cameraNode.camera = SCNCamera()
+        scene.rootNode.addChildNode(cameraNode)
+
+        // place the camera
+        cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
+
+        // create and add a light to the scene
+        let lightNode = SCNNode()
+        lightNode.light = SCNLight()
+        lightNode.light!.type = .omni
+        lightNode.position = SCNVector3(x: 0, y: 10, z: 10)
+        scene.rootNode.addChildNode(lightNode)
+
+        // create and add an ambient light to the scene
+        let ambientLightNode = SCNNode()
+        ambientLightNode.light = SCNLight()
+        ambientLightNode.light!.type = .ambient
+        ambientLightNode.light!.color = UIColor.darkGray
+        scene.rootNode.addChildNode(ambientLightNode)
+
+        // retrieve the ship node
+        let ship = scene.rootNode.childNode(withName: "Plane", recursively: false)!
+
+        // animate the 3d object
+        ship.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 1)))
+
+        self.arkitView.scene = scene
+
+        // allows the user to manipulate the camera
+        self.arkitView.allowsCameraControl = true
+
+        // show statistics such as fps and timing information
+        self.arkitView.showsStatistics = true
+    }
+
 }
